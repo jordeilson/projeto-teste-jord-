fee3f2a295e604577e706264013e41f6b0e122cf
Jordeilson Silva Lima <jordeilsonlima9@gmail.com>
2024-12-28 11:32:48 -0300

    ajustes

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('Olá! esse é o meu primeiro projeto')
+print('Olá')
+
+def calculart_preco_combo(pizza, refrigerente):
+    total = pizza + refrigerante
+    print(total)
+
+calcular_preco_combo(30, 20)
+printnt('Programa finalizado')
+
+# F9 - Marcar um breakpoint
+# F5 - Abrir modo debug
+# F10 - Passar sobre uma linha, SEM entrar em métodos internos
+# F11 - Passassar sobre uma lin ha e ENTRAR em métodos internos
